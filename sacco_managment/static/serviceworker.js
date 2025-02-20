d45a8cd1ec34e895ff6231ad7a484017bed5d9d8
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-02-20 13:38:25 +0300

    Update serviceworker.js

--- sacco_managment/static/serviceworker.js
+++ sacco_managment/static/serviceworker.js
@@ -1,25 +1,3 @@
-// self.addEventListener('install', function(event) {
-//     event.waitUntil(
-//         caches.open('django-pwa-cache').then(function(cache) {
-//             return cache.addAll([
-//                 '/',
-//                 '/static/images/icon-192x192.png',
-//                 '/static/images/icon-512x512.png',
-//                 '/static/css/styles.css',
-//                 '/static/js/main.js'
-//             ]);
-//         })
-//     );
-// });
-
-// self.addEventListener('fetch', function(event) {
-//     event.respondWith(
-//         caches.match(event.request).then(function(response) {
-//             return response || fetch(event.request);
-//         })
-//     );
-// });
-
 const CACHE_NAME = 'Prime SACCO';
 const ASSETS = [
   '/',
