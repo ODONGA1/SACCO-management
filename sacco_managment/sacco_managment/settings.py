1ce17b62b0239eb99cf83cd59dcea63a48620fe9
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-04-13 21:02:01 +0300

    update

--- sacco_managment/sacco_managment/settings.py
+++ sacco_managment/sacco_managment/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.humanize",
+    'django.contrib.sites',
+    
+    
+\
     
     # My Apps
     "core",
@@ -106,6 +110,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                #  'django.template.context_processors.request'
             ],
         },
     },
